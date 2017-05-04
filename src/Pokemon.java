0e267da23df861cc5fe572eff50891cdc498d672
Isabella Nigro <isabella.nigro@usp.br>
2017-05-04 09:33:41 -0300

    Definindo classe pokemon

---
+++ src/Pokemon.java
@@ -0,0 +1,17 @@
+
+public class Pokemon {
+	//nome do pokemon
+	private String nome;
+	//cada pokemon tem ate 4 ataques diferentes
+	private String ataque1;
+	private String ataque2;
+	private String ataque3;
+	private String ataque4;
+	//vida do pokemon
+	private int HP;
+	//cada ataque tem uma quantidade de dano
+	private int danoat1;
+	private int danoat2;
+	private int danoat3;
+	private int danoat4;	
+}

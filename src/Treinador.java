2d61b53c0830e0c86e46fe7b88ef7e18b2a3cf8c
guilhermeyuiti <guilhermee.yuiti@gmail.com>
2017-05-04 09:49:12 -0300

    Classe Treinador arrumada

--- src/Treinador.java
+++ src/Treinador.java
@@ -1,6 +1,15 @@
 
 public class Treinador {
 	private int numpokemons;
-	private String Pokemon1, Pokemon2, Pokemon3, Pokemon4;
-	
+	private String[] pokemon = new String[6];
+	private String treinador;
+	Treinador (int numpokemons, String pokemon[]) {
+		this.numpokemons=numpokemons;
+		this.pokemon[0]=pokemon[0];
+		this.pokemon[1]=pokemon[1];
+		this.pokemon[2]=pokemon[2];
+		this.pokemon[3]=pokemon[3];
+		this.pokemon[4]=pokemon[4];
+		this.pokemon[5]=pokemon[5];
+	}
 }

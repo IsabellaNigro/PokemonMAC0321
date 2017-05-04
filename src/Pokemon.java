7fd8567762e3c66e2a26888be50cf3d5b35719d2
Isabella Nigro <isabella.nigro@usp.br>
2017-05-04 10:43:31 -0300

    criando classe abstrata pokemon

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,17 +1,27 @@
 
-public class Pokemon {
+abstract class Pokemon {
 	//nome do pokemon
 	private String nome;
 	//cada pokemon tem ate 4 ataques diferentes
-	private String ataque1;
-	private String ataque2;
-	private String ataque3;
-	private String ataque4;
+	private String ataque[] = new String[4]; 
 	//vida do pokemon
 	private int HP;
 	//cada ataque tem uma quantidade de dano
-	private int danoat1;
-	private int danoat2;
-	private int danoat3;
-	private int danoat4;	
+	private String dano[] = new String[4];
+	//cada pokemon tem um tipo
+	private String tipo;
+	//construtor
+	Pokemon(String nome, int HP, String tipo, String ataque[], String dano[]){
+		this.nome = nome;
+		this.HP = HP;
+		this.tipo = tipo;
+		this.ataque[0] = ataque[0];
+		this.ataque[1] = ataque[1];
+		this.ataque[2] = ataque[2];
+		this.ataque[3] = ataque[3];
+		this.dano[0] = dano[0];
+		this.dano[1] = dano[1];
+		this.dano[2] = dano[2];
+		this.dano[3] = dano[3];
+	}
 }

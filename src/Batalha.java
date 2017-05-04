8e4a527093268bdcadd6c9071ce007d8261304a0
guilhermeyuiti <guilhermee.yuiti@gmail.com>
2017-05-04 12:05:09 -0300

    Novos pokemons

--- src/Batalha.java
+++ src/Batalha.java
@@ -5,6 +5,8 @@ public class Batalha {
 		Trash T2 = new Trash ();
 		Pikachu P1 = new Pikachu ();
 		Bulbasaur P2 = new Bulbasaur();
-		
+		Charmander P3 = new Charmander();
+		Onix P4 = new Onix ();
+		Graveler P5 = new Graveler(); 
 	}
 }

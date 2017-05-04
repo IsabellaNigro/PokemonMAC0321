ff55d6c58fcb556cc7f89fed09a6574c3a93e428
guilhermeyuiti <guilhermee.yuiti@gmail.com>
2017-05-04 11:50:07 -0300

    Novos objetos

--- src/Batalha.java
+++ src/Batalha.java
@@ -3,5 +3,8 @@ public class Batalha {
 	public static void main(String[] args) {
 		Ash T1 = new Ash ();
 		Trash T2 = new Trash ();
+		Pikachu P1 = new Pikachu ();
+		Bulbasaur P2 = new Bulbasaur();
+		
 	}
 }

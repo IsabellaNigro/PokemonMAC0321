cacf5d4b50296a514702b9e7d7624ed5f7d03d6b
guilhermeyuiti <guilhermee.yuiti@gmail.com>
2017-05-06 18:34:38 -0300

    Novos pokemons

---
+++ src/Cyndaquil.java
@@ -0,0 +1,6 @@
+
+public class Cyndaquil extends Pokemon {
+	public Cyndaquil(){
+		super ("Cyndaquil", 90, "Fogo", new String[]{"Roda de Fogo", "Lança-Chamas", "Cortina de Fumaça", "Agilidade"}, new int[]{15,25,35,30});
+	}
+}
---
+++ src/Ninetales.java
@@ -0,0 +1,6 @@
+
+public class Ninetales extends Pokemon {
+	public Ninetales(){
+		super ("Ninetales", 105, "Fogo", new String[]{"Giro de Fogo", "Raio Confusão", "Confusão", "Ataque Rápido"}, new int[]{25,25,40,30});
+	}
+}

19e80dae714e9eaebad1de2dc56386d0cbd7a0a8
Isabella Nigro <isabella.nigro@usp.br>
2017-05-06 18:43:37 -0300

    mais um pokemon e mais um treinador

---
+++ src/Brock.java
@@ -0,0 +1,27 @@
+
+public class Brock extends Treinador {
+	public Brock() { 
+		super("Brock", 6, new String[] {"Pikachu", "Bulbasauro", "Charmander", "Onix", "Graveler", "Pidgeotto"});
+	}
+	
+	public void atacar(int num, Pokemon inimigo) {
+		System.out.println("Ataque do Brock realizado com sucesso!");
+		inimigo.pokemonAtaca(num, inimigo);
+	}
+	public void trocarpokemon() {
+		System.out.println("Brock trocou seu Pokemón!");
+	}
+	public void usaritem() {
+		System.out.println("Brock usou um item de cura!");
+	}
+	public void fugir() {
+		System.out.println("Brock fugiu da batalha!");
+	}
+	
+	public void usaritem(Pokemon P) {
+		System.out.println("Brock usou um item de cura!");
+		if (P.vivoOuMorto() == true && P.getHPPokemon() + 25 < P.getHPmax())
+			P.SetHP(25);
+	}
+	
+}
---
+++ src/Dratini.java
@@ -0,0 +1,6 @@
+
+public class Dratini extends Pokemon{
+	public Dratini(){
+		super ("Dratini", 110, "Dragao", new String[]{"Ciclone", "Furia do Dragao", "Rabo de Dragao", "Sopro do Dragao"}, new int[]{20, 40 , 25, 30});
+	}
+}

83f8ff734204d0d6ab2946e8cf160c68ab7290be
guilhermeyuiti <guilhermee.yuiti@gmail.com>
2017-05-11 09:54:53 -0300

    Arrumando pokemons

--- src/Raichu.java
+++ src/Raichu.java
@@ -1,7 +1,7 @@
 
 public class Raichu extends Pokemon{
 	public Raichu(){
-		super ("Raichu", 120, "Elétrico", new String[]{"ataque eletrico", "bola eletrica", "raio de gosma", "cauda de aco"}, new int[]{50,50,50,50});
+		super ("Raichu", 120, "Elétrico", new String[]{"Ataque Elétrico", "Bola elétrica", "Raio de Eletróns", "Cauda de Choque"}, new int[]{50,50,50,50});
 	}
 	
 }

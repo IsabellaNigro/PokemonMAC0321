a7a4baf35d5096e8dff71f7375d900c8b096fe9c
Isabella Nigro <isabella.nigro@usp.br>
2017-05-11 11:14:27 -0300

    implementando dano diferenciado

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -53,6 +53,10 @@ abstract class Pokemon {
 		return nome;
 	}
 	
+	public double getMatrizdeDano(int tipoatual, int tipoinimigo){
+		return tabeladedanos[tipoatual][tipoinimigo];
+	}
+	
 	public int getHPPokemon(){
 		return HP;
 	}
@@ -79,11 +83,30 @@ abstract class Pokemon {
 	}
 	public void pokemonAtaca(int num, Pokemon inimigo, Pokemon atual){
 		if (num >=1 && num<=4){
-			if (inimigo.getHPPokemon()-dano[num-1]>0)
-				System.out.println("O pokemon "+inimigo.getNomePokemon()+" está com "+inimigo.SetHP(-(dano[num-1]))+"/"+inimigo.getHPmax()+" de HP");
-			else
+			if (inimigo.getHPPokemon()-dano[num-1]>0){
+				System.out.println("O pokemón "+inimigo.getNomePokemon()+" está com "+inimigo.SetHP((int)(-(atual.getMatrizdeDano(atual.getTipo(), inimigo.getTipo())*(dano[num-1]))))+"/"+inimigo.getHPmax()+" de HP");
+				System.out.println("blah blah blah "+(atual.getMatrizdeDano(atual.getTipo(), inimigo.getTipo())));
+				if (atual.getMatrizdeDano(atual.getTipo(), inimigo.getTipo()) == 0.0)
+					System.out.println("O ataque não funcionou!");
+				if (atual.getMatrizdeDano(atual.getTipo(), inimigo.getTipo()) == 0.5)
+					System.out.println("O ataque não foi muito efetivo!");
+				if (atual.getMatrizdeDano(atual.getTipo(), inimigo.getTipo()) == 1.0)
+					System.out.println("Dano normal");
+				if (atual.getMatrizdeDano(atual.getTipo(), inimigo.getTipo()) == 2.0)
+					System.out.println("Ataque muito efetivo!!!!!");
+				}
+			
+			else{
 				System.out.println("O pokemón "+inimigo.getNomePokemon()+" ficou com "+inimigo.SetHP(-inimigo.getHPPokemon())+" de HP e morreu!!");
-
+				if (atual.getMatrizdeDano(atual.getTipo(), inimigo.getTipo()) == 0.0)
+				System.out.println("O ataque não funcionou!");
+				if (atual.getMatrizdeDano(atual.getTipo(), inimigo.getTipo()) == 0.5)
+				System.out.println("O ataque não foi muito efetivo!");
+				if (atual.getMatrizdeDano(atual.getTipo(), inimigo.getTipo()) == 1.0)
+				System.out.println("Dano normal");
+				if (atual.getMatrizdeDano(atual.getTipo(), inimigo.getTipo()) == 2.0)
+				System.out.println("Ataque muito efetivo!!!!!");
+				}
 		}
 	}
 	
